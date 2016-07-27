a0535f2b7611c698a93ddb1ee7cf618bc984626a
Mark Yeh <Mark Yeh>
2016-07-26 21:50:09 -0700

    first upload

---
+++ src/homework1/problem4.java
@@ -0,0 +1,18 @@
+package homework1;
+
+import java.util.Scanner;
+
+public class problem4 {
+
+	public static void main(String[] args) {
+		System.out.println("Please enter your name:");
+		Scanner in = new Scanner (System.in);
+		String x = in.next();
+		int y =in.nextInt();
+		System.out.printf("Hello, my name is %s and I am %d years old.", x, y);
+		
+		
+
+	}
+
+}
